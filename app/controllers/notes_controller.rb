47d9b36e79edae2123221b56aaf596420d4b4ed1
DanHazard <daniel.love.jr@gmail.com>
2020-07-28 20:42:28 -0500

    Initial backend setup

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,12 @@
+class NotesController < ApplicationController
+
+  def index
+    notes = Note.all
+    render json: NoteSerializer.new(notes)
+  end
+
+  def show
+    note = Note.find(params[:id])
+    render json: NoteSerializer.new(note)
+  end
+end
